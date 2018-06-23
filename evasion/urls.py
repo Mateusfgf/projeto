b5632e138e98b2c473fceb8a7c0f0cf8fba3bc07
Mateusfgf <mateus-medeiross@hotmail.com>
2018-06-23 19:41:09 -0300

    json

--- evasion/urls.py
+++ evasion/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
 		url(r'^evasion/(?P<pk>\d+)/$', views.listar_graficos, name='listar_graficos'),
 		url(r'^form/$', views.formulario_usuario, name='formulario_usuario'),
 		url(r'^cadastro/$', Criar.as_view(), name='cadastro'),
-		url(r'^lista/$', Lista.as_view(), name='lista')
+		url(r'^lista/$', Lista.as_view(), name='lista'),
+		url(r'^events/$', views.events, name='events')
 		# url(r'^cadastro/$', views.Criar, name='cadastro'),
 		# url(r'^lista/$', views.Lista, name='lista')
 
--- evasion/views.py
+++ evasion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import CreateView, ListView
 from django.core.urlresolvers import reverse_lazy
-# from django.HttpResponseRedirect import JsonRespnse
+from django.http import HttpResponseRedirect, Http404, HttpResponseNotFound, HttpResponse, JsonResponse
 
 from evasion.models import Inscricao, Aluno
 from evasion.forms import InscricaoForm
@@ -36,6 +36,51 @@ def listar_graficos(request):
 
 def formulario_usuario(request):
 	return render(request, 'evasion/formulario_usuario.html')
+
+
+
+# def events(request):
+
+# 	{
+#   "events": [
+#     { "id": 1, "name": "Campus Party", 
+#       "description": "Com nove anos de eventos, a Campus Party reúne mais de 8.000 participantes, sendo considerado o maior evento de tecnologia do mundo.", 
+#       "contactInfo": "Em caso de dúvidas, entre em contato com informatica@ifsc.edu.br" },
+#     { "id": 2, "name": "PICNIC Brasil", "description": "Festival de inovação, tecnologia e criatividade, criado em 2006 em Amsterdam, que teve a sua primeira edição internacional em 2016 no Brasil",
+#       "contactInfo": "Em caso de dúvidas, entre em contato com informatica@ifsc.edu.br" },
+
+#     { "id": 3, "name": "Canal do Boi", "description": "Empresas de atuação no agronegócio já reservaram espaço no local da feira. A programação inclui leilões, palestras, shopping de animais, e exposição de máquinas e implementos agropecuários, nos segmentos de nutrição animal, veículos leves e pesados e produtos veterinários.",
+#       "contactInfo": "Em caso de dúvidas, entre em contato com alimentos@ifsc.edu.br" }
+#   ],
+#   "talks": [
+#     { "id": 1, "name": "HACKATHON", "eventId": 1, "day":"05/10", "speaker": "Programadores, designers, profissionais da comunicação, de desenvolvimento de software", "time": "19h00" },
+#     { "id": 2, "name": "Alimentos 2", "eventId": 1, "day":"05/10",  "speaker": "Palestrante 2 Alimentos", "time": "20h30" },
+#     { "id": 3, "name": "Informática 1", "eventId": 1, "day":"06/10",  "speaker": "Palestrante 1 Informática", "time": "08h00" },
+#     { "id": 4, "name": "Informática 2", "eventId": 1, "day":"06/10",  "speaker": "Palestrante 2 Informática", "time": "09h30" },
+#     { "id": 5, "name": "Informática 3", "eventId": 1, "day":"07/10",  "speaker": "Palestrante 3 Informática", "time": "10h00" },
+#     { "id": 6, "name": "Informática 4", "eventId": 2, "day":"07/10",  "speaker": "Palestrante 4 Informática", "time": "11h30" },
+#     { "id": 7, "name": "Informática 5", "eventId": 2, "day":"06/10",  "speaker": "Palestrante 5 Informática", "time": "12h00" },
+#     { "id": 8, "name": "Informática 6", "eventId": 2, "day":"06/10",  "speaker": "Palestrante 6 Informática", "time": "13h30" },
+#     { "id": 9, "name": "Informática 7", "eventId": 2, "day":"07/10",  "speaker": "Palestrante 7 Informática", "time": "14h00" },
+#     { "id": 10, "name": "Informática 8", "eventId": 2, "day":"07/10",  "speaker": "Palestrante 8 Informática", "time": "15h30" },
+#     { "id": 11, "name": "Informática 9", "eventId": 3, "day":"07/10",  "speaker": "Palestrante 9 Informática", "time": "16h30" },
+#     { "id": 12, "name": "Informática 10", "eventId": 3, "day":"06/10",  "speaker": "Palestrante 10 Informática", "time": "17h00" },
+#     { "id": 13, "name": "Informática 11", "eventId": 3, "day":"06/10",  "speaker": "Palestrante 11 Informática", "time": "18h30" },
+#     { "id": 14, "name": "Informática 12", "eventId": 3, "day":"07/10",  "speaker": "Palestrante 12 Informática", "time": "19h00" }
+#   ]
+# }
+
+def events(request):
+    try:
+        post_json = {
+            'status': 1,
+            'id': 1,
+            'description': "Com nove anos de eventos, a Campus Party reúne mais de 8.000 participantes, sendo considerado o maior evento de tecnologia do mundo.",
+            'contactInfo': "Em caso de dúvidas, entre em contato com informatica@ifsc.edu.br",
+        }
+    except ObjectDoesNotExist:
+        post_json = {'status': 0}
+    return JsonResponse(post_json)
 
 
 
